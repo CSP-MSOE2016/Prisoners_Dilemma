fe7c7487f2b15647ccd7be87dab11a452058595d
JakePo50 <jake.popanda@badger.k12.wi.us>
2016-06-08 14:08:02 -0500

    Team 2 Ready
    Changed team2 to choose randomly from the choices of collude or betray.

--- team2.py
+++ team2.py
@@ -1,3 +1,5 @@
+''' Team 2 '''
+
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -5,7 +7,7 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
+import random
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
@@ -25,8 +27,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    options = ['c', 'b']
+    return random.choice(options)
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
