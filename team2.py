9f015c9d4ce57d7567c3bdfe0f90bdd016eb759e
JakePo50 <jake.popanda@badger.k12.wi.us>
2016-06-08 14:14:03 -0500

    team2-random
    Team 2 now chooses randomly between 'c' and 'b'

--- team2.py
+++ team2.py
@@ -8,9 +8,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 import random
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Randomo' # Only 10 chars displayed.
+strategy_name = 'random'
+strategy_description = 'randomly decide to collude or betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
